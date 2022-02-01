5c9fb23bc7d65777afc8d4f384027eff2fe2edc0
Svvetlana <77195728+Svvetlana@users.noreply.github.com>
2022-02-01 20:26:13 +0200

    Update script.js
    change

--- lesson-23/loader/script.js
+++ lesson-23/loader/script.js
@@ -1,14 +1,14 @@
-const { runMain } = require("module");
-
 function download() {
     const xhr = new XMLHttpRequest();
     xhr.open("get", "http://127.0.0.1.:5501/fetch.mp4")
-    xhr.onprogress = function ({ Loaded }) {
+    xhr.onprogress = function ({ loaded }) {
         const max = (+xhr.getResponseHeader('content-length') / 1000000.toFixed(0);
-        const current = (Loaded / 1000000).toFixed;
+        const current = (loaded / 1000000).toFixed;
         data.innerText = `${current} / ${max}`
     }
     xhr.send();
 }
 
 run.onclick = download;
+
+//добавить в папку loader файл fetch.mp4
